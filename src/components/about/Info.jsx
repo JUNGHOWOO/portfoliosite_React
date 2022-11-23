ac2c054fbebbe0ac8ae50f8a992a33a353e239e2
DESKTOP-VSRUVEK\우정호 <lemondrop0212@naver.com>
2022-11-23 20:18:38 +0900

    기본적인 React 프로젝트 구성. home, header, scoll, social, data, about jsx 등 추가

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./about.css";
+import AboutImg from "../../assets/about.jpg";
+import CV from "../../assets/John-Cv.pdf";
+
+const Info = () => {
+    return (
+        <section className="about section" id="about">
+            
+        </section>
+    )
+}
+
+export default Info
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://www.instagram.com/99wo_ow/" 
+            className="home__social-icon" target="_blank">
+                <i class = "uil uil-instagram"></i>
+            </a>
+            <a href="" className="home__social-icon" target="_blank">
+                <i class = "uil uil-adjust-circle"></i>
+            </a>
+            <a href="" className="home__social-icon" target="_blank">
+                <i class = "uil uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
