96a24294fb3b1181e6e719feff725a4742950573
JUNGHOWOO <lemondrop0212@naver.com>
2022-11-25 16:58:57 +0900

    example typed effect 추가_home inro 객체

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import "./home.css";
 
 const Data = () => {
+    const txt = "hello, My name is";
+    const [Text, setText] = useState('');
+    const [Count, setCount] = useState(0);
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+          setText(Text + txt[Count]); // 이전 set한 문자 + 다음 문자
+          setCount(Count + 1); // 개수 만큼 체크 
+      }, 100);
+      if(Count === txt.length)  {  // Count를 따로 두지 않고 Text.length 체크도 가능
+          clearInterval(interval); // 문자열 체크를 통해 setInterval을 해제합니다
+      }
+      return () => clearInterval(interval); // 언마운트시 setInterval을 해제합니다
+  })  
+          
     return (
         <div className="home__data">
+            <h1 className='home__intro'>{ Text }</h1>
             <h1 className="home__title">Jungho Woo
             <svg
                   width="36"
