28f4f77eba1601ce20e05b24e657881220240fac
DESKTOP-VSRUVEK\우정호 <lemondrop0212@naver.com>
2022-12-01 00:47:15 +0900

    인트로 화면 수정 시도

--- src/router/Links.js
+++ src/router/Links.js
@@ -16,3 +16,27 @@ export default function Links() {
     
   )
 }
+
+{/* <section className="skills section" id="skills">
+<h2 className="section__title">정호의 포트폴리오 사이트에 오신 것을 환영합니다 !<br/>
+welcome to jungho's portfolio site !</h2>
+<span className="section__subtitle">언어를 선택하여 주세요. <br/>
+chose languages</span>
+<div className="skills__container container grid">
+</div>
+
+
+<div class="intro">
+<div class="loader">
+<div class="box">
+<div className="btn_kr">
+<Link to="/kr">korea</Link>
+</div>
+<div class="loader6"></div>
+<div className="btn_en">
+<Link to="/en">english</Link>
+</div>
+</div>
+</div>
+</div>
+</section> */}
